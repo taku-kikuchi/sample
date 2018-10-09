c93e35eb2ec9e92edeee6abe37bc15729cd48774
y-nakatani <y-nakatani@winas.jp>
2018-10-09 15:40:59 +0900

    とりあえずコミットテスト

---
+++ dentaku.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lag="ja">
+<head >
+<meta charset="utf-8">
+</head>
+<body>
+<form action="" method="POST">
+<button type="submit" name="num" value="1">1</button>
+<button type="submit" name="num" value="2">2</button>
+<button type="submit" name="num" value="3">3</button>
+<button type="submit" name="num" value="4">4</button>
+<button type="submit" name="num" value="5">5</button>
+</form>
+<?php
+if(isset($_POST['num'])){
+$_POST['sum'] = "";
+}else{
+$_POST['sum'] = $_POST['sum'].$_POST['num'];
+echo $_POST['sum'];
+}
+?>
+</body>
+</html>
